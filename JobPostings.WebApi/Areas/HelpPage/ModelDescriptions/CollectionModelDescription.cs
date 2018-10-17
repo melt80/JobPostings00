5d4c819e1b8600b77d332fa234c4f0e43e4b557c
Neil Tiedeman <neilt@knowledgeservices.com>
2018-10-17 09:49:00 -0400

    Add project files.

---
+++ JobPostings.WebApi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace JobPostings.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ JobPostings.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="JobPostings.WebApi.WebApiApplication" Language="C#" %>
